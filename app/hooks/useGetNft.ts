0410a0ac7524909d3af2d8304de31483900a5fa6
Abhishekkochar <abhishekkochar2@gmail.com>
2024-05-30 16:06:03 +1000

    Added support for env
    Signed-off-by: Abhishekkochar <abhishekkochar2@gmail.com>

--- app/hooks/useGetNft.ts
+++ app/hooks/useGetNft.ts
@@ -1,8 +1,8 @@
 import { Alchemy, Network} from "alchemy-sdk";
-import { apiKey, pudgyPenguinContract } from "../utils/constants";
+import { pudgyPenguinContract } from "../utils/constants";
 
 const config = {
-    apiKey: apiKey,
+    apiKey: process.env.API,
     network: Network.ETH_MAINNET
     }
 
@@ -12,6 +12,7 @@ export default async function useGetNft( inputAddress:string, setNftList:(list: 
     try{
         let nfts = await alchemy.nft.getNftsForOwner(inputAddress, {contractAddresses: [pudgyPenguinContract]});
         setNftList(nfts.ownedNfts)
+        console.log(process.env.API)
     } catch(e){
         console.log(e)
     }
